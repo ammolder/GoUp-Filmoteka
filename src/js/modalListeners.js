5662469d7904ad9c622c833b339f480ce1df89fb
linluxx <karolina.kkv@gmail.com>
2022-09-30 00:50:58 +0300

    fixed sections

--- src/js/modalListeners.js
+++ src/js/modalListeners.js
@@ -20,11 +20,16 @@ export async function onClickCard(evt) {
 export function onClickModal(evt) {
   console.log(evt.target);
   const modalOverlay = document.querySelector('.backdrop');
-  modalOverlay.addEventListener('click', onClickModal);
+  // modalOverlay.addEventListener('click', onClickModal);
   if (evt.target === modalOverlay) {
     modalOverlay.remove();
     document.removeEventListener('keydown', keyDown);
   }
+}
+
+export function onBtnCloseClick() {
+  const modalBackdrop = document.querySelector('.backdrop');
+  modalBackdrop.remove();
 }
 
 export function keyDown(evt) {
@@ -34,4 +39,22 @@ export function keyDown(evt) {
     modalOverlay.remove();
     document.removeEventListener('keydown', keyDown);
   }
+}
+
+export function onWatchedClick(evt) {
+  button = evt.currentTarget;
+  button.textContent = 'REMOVE FROM WATCHED';
+  button.style.backgroundColor = 'red';
+  button.style.outline = 'none';
+  button.style.borderColor = 'transparent';
+  button.style.color = 'var(--primary-text-color)';
+}
+
+export function onQueueClick(evt) {
+  button = evt.currentTarget;
+  button.textContent = 'REMOVE FROM QUEUE';
+  button.style.backgroundColor = 'red';
+  button.style.outline = 'none';
+  button.style.borderColor = 'transparent';
+  button.style.color = 'var(--primary-text-color)';
 }
