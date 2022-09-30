53302500aefb414f6a9f3a8bb62b958727a05b92
VladSalash <salashchenko99@gmail.com>
2022-09-30 23:37:53 +0300

    submitLoader

--- src/js/searchApi.js
+++ src/js/searchApi.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
+import {displayLoading} from './loader/submitSpinner.js'
+
+
 export async function getApi(name) {
   const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
   const params = {
     api_key: '7f524807c48f906ff0108130fa25f727',
     query: `${name}`,
   };
-
+  displayLoading()
   const resolve = await axios(BASE_URL, { params });
   return resolve;
 }
