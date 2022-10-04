f6d1d4b3c3934202a4b28391bcac4a77e1149a4c
ammolder <98980721+ammolder@users.noreply.github.com>
2022-10-04 18:51:32 +0200

    delete/console

--- src/js/localstorageLibrary.js
+++ src/js/localstorageLibrary.js
@@ -7,7 +7,6 @@ export const refs = {
   emptyWrap: document.querySelector('.library__empty-wrap'),
 };
 let watchedLibraryList = JSON.parse(localStorage.getItem(STORAGE_WATCHED_KEY));
-console.log(watchedLibraryList);
 let queueLibraryList = JSON.parse(localStorage.getItem(STORAGE_QUEUE_KEY));
 if (refs.watched) {
   refs.watched.addEventListener('click', onLibraryWatchedClick);
